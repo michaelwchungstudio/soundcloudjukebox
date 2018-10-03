3640eb6082c64ffdf3feead79eef46fc8324ec68
Michael W Chung <dryhslaw@gmail.com>
2018-10-03 14:41:14 -0400

    updated songs and albums to vary

--- index.js
+++ index.js
@@ -20,27 +20,22 @@ class Song {
 }
 
 // Creating a pre-made list of Song objects
-var s1 = new Song("Amenamy", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Amenamy.mp3");
-var s2 = new Song("Belispeak", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Belispeak.mp3");
-var s3 = new Song("Cartographist", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Cartographist.mp3");
-var s4 = new Song("Crawlersout", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Crawlersout.mp3");
-var s5 = new Song("Father Stretch My Hands Pt. 1 feat. Kid Cudi", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/FatherStretchMyHandsPt1.mp3");
-var s6 = new Song("Fineshrine", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Fineshrine.mp3");
-var s7 = new Song("FML feat. The Weeknd", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/fml.mp3");
-var s8 = new Song("I Love Kanye", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/ilovekanye.mp3");
-var s9 = new Song("Lofticries", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Lofticries.mp3");
-var s10 = new Song("Obedear", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Obedear.mp3");
-var s11 = new Song("Saltkin", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Saltkin.mp3");
-var s12 = new Song("Shuck", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Shuck.mp3");
-var s13 = new Song("30 Hours feat. Andre 3000", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/thirtyhours.mp3");
-var s14 = new Song("Ultralight Beam feat. Chance the Rapper & Kirk Franklin", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/UltraLightBeam.mp3");
-var s15 = new Song("Ungirthed", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Ungirthed.mp3");
-var s16 = new Song("Famous feat. Rihanna", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/famous.mp3");
+var s1 = new Song("My Girls", "Animal Collective", "Merriweather Post Pavilion", "img/merriweather.jpg", "audio/animalcollecmygirls.mp3");
+var s2 = new Song("Child I Will Hurt You", "Crystal Castles", "(III)", "img/iii.jpg", "audio/crystalcastleschild.mp3");
+var s3 = new Song("Ultimate", "Denzel Curry", "32 Zel/Planet Shrooms", "img/planetshrooms.jpg", "audio/denzelultimate.mp3");
+var s4 = new Song("Lucky You feat. Joyner Lucas", "Eminem", "Kamikaze", "img/kamikaze.png", "audio/emluckyyou.mp3");
+var s5 = new Song("Fineshrine", "Purity Ring", "Shrines", "img/shrines.jpg", "audio/Fineshrine.mp3");
+var s6 = new Song("Two Weeks", "FKA Twigs", "LP1", "img/lp1.jpg", "audio/fkatwoweeks.mp3");
+var s7 = new Song("SLOW DANCING IN THE DARK", "Joji", "SLOW DANCING IN THE DARK", "img/slowdancing.jpg", "audio/jojislowdancing.mp3");
+var s8 = new Song("Heartbeats", "The Knife", "Deep Cuts", "img/deepcuts.jpg", "audio/knifeheartbeats.mp3");
+var s9 = new Song("Ultralight Beam feat. Chance the Rapper & Kirk Franklin", "Kanye West", "The Life of Pablo", "img/tlop.jpg", "audio/UltraLightBeam.mp3");
+var s10 = new Song("Intro", "The xx", "xx", "img/xx.png", "audio/xxintro.mp3");
+var s11 = new Song("Heart Attack", "Scarlxrd", "Chaos Theory", "img/chaostheory.jpg", "audio/heartattack.mp3")
 
 // Jukebox class that consists of an array of the previously made Song objects
 class Jukebox {
   constructor() {
-    this.songArchive = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16];
+    this.songArchive = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11];
     this.trackNum = 0;
     this.currentSong = new Audio(this.songArchive[this.trackNum].url);
     // this.currentSongName = this.songArchive[this.trackNum].songtitle;
